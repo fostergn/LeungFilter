2263a5c2f69044b9af4e556d0b9188f80177e6ec
Gavin Foster <fostergn@vcu.edu>
2015-11-04 15:40:51 -0500

    adding functions to select nav and select individual drawing

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,11 +2,29 @@
 angular.module('mainController', ['wu.masonry'])
     // inject the Todo service factory into our controller
     .controller('homeCtrl', function($scope, Drawings) {
-        var categories = ["food", "feet", "form", "text", "threads", "people", "plumes", "other"];
-        var random = Math.floor(Math.random() * categories.length);
-        $('nav li:nth-child('+(random+1)+')').addClass('selected-nav');
-        Drawings.getCategory(categories[random])
+        $scope.categories = ["food", "feet", "form", "text", "threads", "people", "plumes", "other"];
+        var random = Math.floor(Math.random() * $scope.categories.length);
+        $scope.selected = $scope.categories[random];
+
+        $scope.selectNav = function(index){
+            $scope.selected = $scope.categories[index];
+            console.log($scope.selected);
+            Drawings.getCategory($scope.selected)
+                .success(function(data){
+                    $scope.drawings = data;
+                });
+        }
+
+        $scope.selectDrawing = function(id){
+            Drawings.getDrawing(id)
+                .success(function(data){
+                    $scope.drawings = data;
+                });
+        }
+        
+        Drawings.getCategory($scope.categories[random])
             .success(function(data){
                 $scope.drawings = data;
             });
+
     });
